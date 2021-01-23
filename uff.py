785a7077b8588a49eaf9b1b66611eaac4b7fa349
Aurelien Gateau <mail@agateau.com>
2021-01-23 10:03:52 +0100

    Add license information

--- uff.py
+++ uff.py
@@ -1,4 +1,6 @@
 #!/usr/bin/env python3
+# Copyright 2021 Aurélien Gâteau <mail@agateau.com>
+# SPDX-License-Identifier: Apache-2.0
 """
 Prints the forge url for a given file or path of a Git repository checkout.
 """

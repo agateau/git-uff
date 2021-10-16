cb5850b01a227377b0dccad4c6c819bb66cae8cf
Aurelien Gateau <mail@agateau.com>
2021-10-16 12:40:16 +0200

    Rename -c, --commit to -p, --permalink
    This leaves -c for the --clipboard (or --copy) option.

--- git_uff/main.py
+++ git_uff/main.py
@@ -72,7 +72,7 @@ def main() -> None:
     parser.add_argument("-b", "--branch",
                         help="Use branch BRANCH instead of the current one",
                         metavar="BRANCH")
-    parser.add_argument("-c", "--commit", action="store_true",
+    parser.add_argument("-p", "--permalink", action="store_true",
                         help="Replace the branch in the URL with the commit"
                         " it points to")
     parser.add_argument("-l", "--line", type=int, help="Line to point to")
@@ -94,7 +94,7 @@ def main() -> None:
             branch = args.branch
         else:
             branch = repo.active_branch.name
-        if args.commit:
+        if args.permalink:
             branch = repo.rev_parse(branch).hexsha
         url = converter.run(remote_url, branch,
                             path.relative_to(repo_root), args.line)
